e3e65e0fcc88e512935ca60a7ae188911627013a
Norman Noe <nnoe@thorntontomasetti.com>
2020-08-12 07:15:21 -0500

    wpf push

--- RemoteTest/RemoteConsole/RemoteConsole/Program.cs
+++ RemoteTest/RemoteConsole/RemoteConsole/Program.cs
@@ -13,26 +13,57 @@ namespace RemoteConsole
     {
         static void Main(string[] args)
         {
-            cOAPI coapi = null;
-            cHelper helper = new Helper();
             int ret = 0;
-            int[] tpc = new int[] { 11650, 49151 };
+
+            //Retrieve current instance
+            cOAPI SapObjectClient = null;
+            cHelper HelperClient = new Helper();
+            SapObjectClient = HelperClient.GetObject("CSI.SAP2000.API.SapObject");
+            cSapModel SapModelClient = SapObjectClient.SapModel;
+
+
+            
+
+            //Save to Pdrive
+
+            //Connect to Server
+
+            //Open file from server from pdrive
+
+            //Save file in different location locally
+
+            //run analysis for a few load cases
+
+            //merge analysis results with file on pdrive
+
+            //return to local
+
+
+
+
+
+
+
+
+            int[] tpc = new int[] { 11650, 49100 };
+
+
+
 
             //Try and run multiple instance on IL1M113 using different TPC ports
-            //Parallel.For(0, 2, i =>
-            //  {
-            //      //IF I WANT TO RUN MULTIPLE INSTANCES ON THE SAME COMPUTER CAN I LOOK UP THE AVAILABLE TCP PORTS ON THAT COMPUTER?  
-            //      //HOW DOES THIS WORK?
+            Parallel.For(0, 2, i =>
+            {
+                  //IF I WANT TO RUN MULTIPLE INSTANCES ON THE SAME COMPUTER CAN I LOOK UP THE AVAILABLE TCP PORTS ON THAT COMPUTER?  
+                  //HOW DOES THIS WORK?
 
+                  coapi = helper.CreateObjectProgIDHostPort("IL1M113", tpc[0], "CSI.SAP2000.API.SapObject");
+                  ret = coapi.ApplicationStart(eUnits.kip_ft_F);
+                  cSapModel SapModel = coapi.SapModel;
+                  ret = SapModel.InitializeNewModel();
 
-            coapi = helper.CreateObjectProgIDHostPort("IL1W015", tpc[0], "CSI.SAP2000.API.SapObject");
-            ret = coapi.ApplicationStart();
-            cSapModel SapModel = coapi.SapModel;
-            ret = SapModel.InitializeNewModel();
-
-            //initialize model
-            //  }
-            //);
+                  //initialize model
+              }
+            );
 
             //Try and run multiple instances on my own computer
             //Parallel.For(0, 2, i =>
---
+++ RemoteTest/RemoteConsole/WPF/MainWindow.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SAP2000v1;
+
+namespace WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+
+            InfillCases.Items.Add(lc);
+
+
+            //write to list
+
+
+
+
+
+            //Retrieve current instance
+            //cOAPI SapObjectClient = null;
+            //cHelper HelperClient = new Helper();
+            //SapObjectClient = HelperClient.GetObject("CSI.SAP2000.API.SapObject");
+            //cSapModel SapModelClient = SapObjectClient.SapModel;
+        }
+
+
+        public static List<LoadCase> PopulateGrid()
+        {
+            //Retrieve current instance
+            cOAPI SapObjectClient = null;
+            cHelper HelperClient = new Helper();
+            SapObjectClient = HelperClient.GetObject("CSI.SAP2000.API.SapObject");
+            cSapModel SapModelClient = SapObjectClient.SapModel;
+
+            int numberofitems = 0;
+            string[] casenames = null;
+            int[] status = null;
+
+            int numberofitemsrun = 0;
+            string[] casenamesrun = null;
+            bool[] run = null;
+
+            int ret = SapModelClient.Analyze.GetCaseStatus(ref numberofitems, ref casenames, ref status);
+            ret = SapModelClient.Analyze.GetRunCaseFlag(ref numberofitemsrun, ref casenamesrun, ref run);
+
+            List<int> statuslist = new List<int>();
+
+            for (int i = 0; i < numberofitems;i++)
+            {
+                LoadCase lc = new LoadCase(casenames[i], status[i], true, "Client");
+                
+            }
+            InfillCases.Items.Add(lc);
+
+
+
+        }
+
+        public static void PopulateDropdown()
+        {
+
+        }
+
+        /// <summary>
+        /// Assign selected grid to a specific drop down
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Confirmbutton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Assign all to a specific drop down
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DoNotRunButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Trigger Run Sequence
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void RunAnalysis_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
